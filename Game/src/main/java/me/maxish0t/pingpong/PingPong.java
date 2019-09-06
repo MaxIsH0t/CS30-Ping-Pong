06fcaa848568afe8dea8072ae4118a1dd078d198
Max Saleh <43427964+MaxIsH0t@users.noreply.github.com>
2019-09-06 11:05:25 -0600

    Starting Work On the main GUI for the ping pong

--- Game/src/main/java/me/maxish0t/pingpong/PingPong.java
+++ Game/src/main/java/me/maxish0t/pingpong/PingPong.java
@@ -1,6 +1,13 @@
 package me.maxish0t.pingpong;
 
+import me.maxish0t.pingpong.gui.MainPingPongGUI;
+
 public class PingPong
 {
+    public static void main(String[] args)
+    {
+        MainPingPongGUI.mainGUI();
 
+        System.out.println("Game has been loaded!");
+    }
 }
---
+++ Game/src/main/java/me/maxish0t/pingpong/gui/MainPingPongGUI.java
@@ -0,0 +1,14 @@
+package me.maxish0t.pingpong.gui;
+
+import javax.swing.*;
+
+public class MainPingPongGUI
+{
+    public static void mainGUI()
+    {
+        JFrame jFrame = new JFrame("PingPong");
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(300, 300);
+        jFrame.setVisible(true);
+    }
+}
