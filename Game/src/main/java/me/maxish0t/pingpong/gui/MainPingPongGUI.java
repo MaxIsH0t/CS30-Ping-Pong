b749e71fa8b585f061b8d98f41b74e28f5e36aa7
Max Saleh <43427964+MaxIsH0t@users.noreply.github.com>
2019-09-06 11:18:51 -0600

    Few touch ups on the GUI

--- Game/src/main/java/me/maxish0t/pingpong/gui/MainPingPongGUI.java
+++ Game/src/main/java/me/maxish0t/pingpong/gui/MainPingPongGUI.java
@@ -1,14 +1,26 @@
 package me.maxish0t.pingpong.gui;
 
+import me.maxish0t.pingpong.util.PingPongUtils;
+
 import javax.swing.*;
 
 public class MainPingPongGUI
 {
+    private static int GUI_WIDTH = 1200;
+    private static int GUI_HEIGHT = 750;
+
+    // Main GUI for the Game
     public static void mainGUI()
     {
-        JFrame jFrame = new JFrame("PingPong");
+        JFrame jFrame = new JFrame(PingPongUtils.GAME_NAME);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(300, 300);
+        jFrame.setSize(GUI_WIDTH, GUI_HEIGHT);
+
+        // Sets the GUI in the middle of the monitor
+        jFrame.setLocationRelativeTo(null);
+
         jFrame.setVisible(true);
+
+        PingPongUtils.LOGGER.info("Game Size: " + GUI_WIDTH + " by " + GUI_HEIGHT);
     }
 }
