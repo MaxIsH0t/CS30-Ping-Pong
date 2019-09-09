93176efd55886f55e15f329d8e13f8e0d51e9737
Max Saleh <43427964+MaxIsH0t@users.noreply.github.com>
2019-09-09 10:48:02 -0600

    Removed not needed imports and code

--- Game/src/main/java/me/maxish0t/pingpong/PingPong.java
+++ Game/src/main/java/me/maxish0t/pingpong/PingPong.java
@@ -3,8 +3,6 @@ package me.maxish0t.pingpong;
 import me.maxish0t.pingpong.gui.MainPingPongGUI;
 import me.maxish0t.pingpong.util.PingPongUtils;
 
-import java.awt.*;
-
 /**
  * Created By Mahmoud Saleh
  * Date Created On: Friday, September 6, 2019
@@ -14,11 +12,7 @@ public class PingPong
     public static void main(String[] args)
     {
         // Loads the main GUI for the Ping Pong Game
-        MainPingPongGUI.mainGUI();
-        Rectangle box= new Rectangle(5,10,20,30);
-        System.out.println(box);
-
-
+        MainPingPongGUI mainPingPongGUI = new MainPingPongGUI();
 
         PingPongUtils.LOGGER.info(PingPongUtils.GAME_NAME + " has been loaded on version " + PingPongUtils.GAME_VERSION);
     }
--- Game/src/main/java/me/maxish0t/pingpong/gui/MainPingPongGUI.java
+++ Game/src/main/java/me/maxish0t/pingpong/gui/MainPingPongGUI.java
@@ -1,6 +1,5 @@
 package me.maxish0t.pingpong.gui;
 
-import me.maxish0t.pingpong.util.DrawUtils;
 import me.maxish0t.pingpong.util.PingPongUtils;
 
 import javax.swing.*;
@@ -12,14 +11,10 @@ public class MainPingPongGUI extends JFrame
     private static int GUI_HEIGHT = 750;
     private static final String GUI_TITLE = "PingPong";
 
-    public MainPingPongGUI(String title) throws HeadlessException {
-        super(title);
-        title = GUI_TITLE;
-    }
-
-    // Main GUI for the Game
-    public static void mainGUI()
+    public MainPingPongGUI()
     {
+        setTitle(GUI_TITLE);
+
         JFrame jFrame = new JFrame(PingPongUtils.GAME_NAME);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setSize(GUI_WIDTH, GUI_HEIGHT);
@@ -29,20 +24,6 @@ public class MainPingPongGUI extends JFrame
 
         jFrame.setVisible(true);
 
-        // Boxes
-        //DrawUtils.drawRectangle(10, 10, 20, 20, Color.BLACK);
-
         PingPongUtils.LOGGER.info("Game Size: " + GUI_WIDTH + " by " + GUI_HEIGHT);
-    }
-
-    public void paint(Graphics g)
-    {
-        Rectangle r = new Rectangle(10, 10, 20, 20);
-        g.fillRect(
-                (int)r.getX(),
-                (int)r.getY(),
-                (int)r.getWidth(),
-                (int)r.getHeight()
-        );
     }
 }
