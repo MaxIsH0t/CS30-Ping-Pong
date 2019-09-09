c03606ff22222f92dde748808661dbbd8f7711ef
Max Saleh <43427964+MaxIsH0t@users.noreply.github.com>
2019-09-09 11:00:29 -0600

    Added a circle in the middle of the screen

--- Game/src/main/java/me/maxish0t/pingpong/gui/MainPingPongGUI.java
+++ Game/src/main/java/me/maxish0t/pingpong/gui/MainPingPongGUI.java
@@ -1,5 +1,6 @@
 package me.maxish0t.pingpong.gui;
 
+import me.maxish0t.pingpong.util.DrawUtils;
 import me.maxish0t.pingpong.util.PingPongUtils;
 
 import javax.swing.*;
@@ -14,16 +15,16 @@ public class MainPingPongGUI extends JFrame
     public MainPingPongGUI()
     {
         setTitle(GUI_TITLE);
-
-        JFrame jFrame = new JFrame(PingPongUtils.GAME_NAME);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(GUI_WIDTH, GUI_HEIGHT);
-
-        // Sets the GUI in the middle of the monitor
-        jFrame.setLocationRelativeTo(null);
-
-        jFrame.setVisible(true);
+        setSize(GUI_WIDTH, GUI_HEIGHT);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         PingPongUtils.LOGGER.info("Game Size: " + GUI_WIDTH + " by " + GUI_HEIGHT);
+    }
+
+    public void paint(Graphics g)
+    {
+        //DrawUtils.drawRectangle(50, 50, 100, 100, Color.RED, g);
+        DrawUtils.drawCircle(getWidth() * 1/2, getHeight() * 1/2, 20, 20, Color.RED, g);
     }
 }
--- Game/src/main/java/me/maxish0t/pingpong/util/DrawUtils.java
+++ Game/src/main/java/me/maxish0t/pingpong/util/DrawUtils.java
@@ -1,11 +1,26 @@
 package me.maxish0t.pingpong.util;
 
-import javax.swing.*;
 import java.awt.*;
 
-public class DrawUtils extends JFrame
+public class DrawUtils
 {
-    public static void drawRectangle(int x, int y, int width, int height, Color color)
+    /**
+     * Draws a Rectangle
+     */
+    public static void drawRectangle(int x, int y, int width, int height, Color color, Graphics graphics)
     {
+        graphics.setColor(color);
+        graphics.drawRect(x, y, width, height);
+        graphics.fillRect(x, y, width, height);
+    }
+
+    /**
+     * Draws a Circle
+     */
+    public static void drawCircle(int x, int y, int width, int height, Color color, Graphics graphics)
+    {
+        graphics.setColor(color);
+        graphics.drawOval(x, y, width, height);
+        graphics.fillOval(x, y, width, height);
     }
 }
