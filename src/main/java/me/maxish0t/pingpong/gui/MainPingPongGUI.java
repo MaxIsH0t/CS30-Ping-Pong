f0cd3df5afb05dca6d153b5eedd19adace58f481
Max Saleh <43427964+MaxIsH0t@users.noreply.github.com>
2019-11-21 09:31:07 -0700

    Added fade menu

--- src/main/java/me/maxish0t/pingpong/gui/MainPingPongGUI.java
+++ src/main/java/me/maxish0t/pingpong/gui/MainPingPongGUI.java
@@ -127,139 +127,147 @@ public class MainPingPongGUI extends JPanel implements KeyListener, ActionListen
             g2d.draw(ball.getEllipse());
         }
 
-        if (!hasPlayedMenu) {
-            String string = "How to play?";
-            String string2 = "- You use the left and right keys on the keyboard.";
-            TextUtils.drawText(string, (width / 2) - (250 / 2), 250, 50, Color.WHITE, g);
-            TextUtils.drawText(string2, (width / 2) - (450), 300, 50, Color.WHITE, g);
-
-            DrawUtils.drawPlayButton("Press Here To Play", (Constants.displayWidth / 2) - (500 / 2), 10, 500, 100, Color.RED, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    hasPlayedMenu = true;
-                    DrawUtils.playButton.setVisible(false);
-                }
-            }, this);
-        } else if (hasPlayedMenu) {
-            /**
-            for (int i = 0; i < fireworks.length; i++) {
-                fireworks[i] = new Blackhole();
-
-                fireworks[i].edgeDetection();
-                fireworks[i].move();
-                fireworks[i].draw(g);
-            }**/
-
-            // initial positioning
-            if (first) {
-                isGameReset = false;
-                bottomPadX = width / 2 - padW / 2;
-                topPadX = bottomPadX;
-                ballX = width / 2 - ballSize / 2;
-                ballY = height / 2 - ballSize / 2;
-                first = false;
+        if(VariablesGUI.timer >= 0) {
+            VariablesGUI.timer--;
+            DrawUtils.drawRectangle(0, 0, width, height, Color.GRAY, g);
+            TextUtils.drawText("Welcome to MaxIsH0t's PingPong Game!", (width / 2) - (200 * 2), height / 2, 50, Color.WHITE, g);
+            if (VariablesGUI.fade < 1.0F) {
+                VariablesGUI.fade += 0.03F;
             }
+        } else {
+            if (!hasPlayedMenu) {
+                String string = "How to play?";
+                String string2 = "- You use the left and right keys on the keyboard.";
+                TextUtils.drawText(string, (width / 2) - (250 / 2), 250, 50, Color.WHITE, g);
+                TextUtils.drawText(string2, (width / 2) - (480), 300, 50, Color.WHITE, g);
 
-            if (isBGBlack == true && isBGWhite == false) {
-                setBackground(Color.BLACK);
+                DrawUtils.drawPlayButton("Press Here To Play", (Constants.displayWidth / 2) - (500 / 2), 10, 500, 100, Color.RED, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        hasPlayedMenu = true;
+                        DrawUtils.playButton.setVisible(false);
+                    }
+                }, this);
+            } else if (hasPlayedMenu) {
+                /**
+                 for (int i = 0; i < fireworks.length; i++) {
+                 fireworks[i] = new Blackhole();
 
-                // bottom pad
-                RenderPaddles bottomPaddle = new RenderPaddles(bottomPadX, height - padH - inset, padW, padH, new Color(R, G, B), g);
-                bottomPaddle.renderPaddle();
+                 fireworks[i].edgeDetection();
+                 fireworks[i].move();
+                 fireworks[i].draw(g);
+                 }**/
 
-                // top pad
-                RenderPaddles topPaddle = new RenderPaddles(topPadX, inset, padW, padH, new Color(R, G, B), g);
-                topPaddle.renderPaddle();
-
-                // ball
-                DrawUtils.drawCircle(ballX, ballY, ballSize, new Color(R, G, B), g);
-
-                TextUtils.drawText(BORDER, 0, MainPingPongGUI.height / 2, 35, new Color(R, G, B), g);
-
-                String scoreT = "AI: " + new Integer(scoreTop).toString();
-                String scoreB = "Player: " + new Integer(scoreBottom).toString();
-                TextUtils.drawText(scoreT, width - 105, height / 2 - 30, 30, Color.WHITE, g);
-                TextUtils.drawText(scoreB, width - 125, height / 2 + 30, 30, Color.WHITE, g);
-            }
-
-            if (isBGWhite == true && isBGBlack == false) {
-                setBackground(Color.WHITE);
-
-                // bottom pad
-                RenderPaddles bottomPaddle = new RenderPaddles(bottomPadX, height - padH - inset, padW, padH, Color.WHITE, g);
-                bottomPaddle.renderPaddle();
-
-                // top pad
-                RenderPaddles topPaddle = new RenderPaddles(topPadX, inset, padW, padH, Color.WHITE, g);
-                topPaddle.renderPaddle();
-
-                // ball
-                DrawUtils.drawCircle(ballX, ballY, ballSize, Color.WHITE, g);
-
-                TextUtils.drawText(BORDER, 0, height / 2, 35, Color.BLACK, g);
-                String scoreT = "AI: " + new Integer(scoreTop).toString();
-                String scoreB = "Player: " + new Integer(scoreBottom).toString();
-                TextUtils.drawText(scoreT, width - 105, height / 2 - 30, 30, Color.BLACK, g);
-                TextUtils.drawText(scoreB, width - 125, height / 2 + 30, 30, Color.BLACK, g);
-            }
-
-            // reset button
-            DrawUtils.drawResetButton("RESET", 10, height / 2 + 30, 120, 50, Color.YELLOW, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
+                // initial positioning
+                if (first) {
+                    isGameReset = false;
+                    bottomPadX = width / 2 - padW / 2;
+                    topPadX = bottomPadX;
                     ballX = width / 2 - ballSize / 2;
                     ballY = height / 2 - ballSize / 2;
-
-                    ballX += ballSpeedX;
-                    ballY += ballSpeedY;
-
-                    scoreBottom = 0;
-                    scoreTop = 0;
+                    first = false;
                 }
-            }, this);
 
-            // exit button
-            DrawUtils.drawExitButton("EXIT", 10, height / 2 + 90, 120, 50, Color.YELLOW, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.exit(5);
+                if (isBGBlack == true && isBGWhite == false) {
+                    setBackground(Color.BLACK);
+
+                    // bottom pad
+                    RenderPaddles bottomPaddle = new RenderPaddles(bottomPadX, height - padH - inset, padW, padH, new Color(R, G, B), g);
+                    bottomPaddle.renderPaddle();
+
+                    // top pad
+                    RenderPaddles topPaddle = new RenderPaddles(topPadX, inset, padW, padH, new Color(R, G, B), g);
+                    topPaddle.renderPaddle();
+
+                    // ball
+                    DrawUtils.drawCircle(ballX, ballY, ballSize, new Color(R, G, B), g);
+
+                    TextUtils.drawText(BORDER, 0, MainPingPongGUI.height / 2, 35, new Color(R, G, B), g);
+
+                    String scoreT = "AI: " + new Integer(scoreTop).toString();
+                    String scoreB = "Player: " + new Integer(scoreBottom).toString();
+                    TextUtils.drawText(scoreT, width - 105, height / 2 - 30, 30, Color.WHITE, g);
+                    TextUtils.drawText(scoreB, width - 125, height / 2 + 30, 30, Color.WHITE, g);
                 }
-            }, this);
 
-            // white button
-            DrawUtils.drawWhiteButton("WHITE", 10, height / 2 - 90, 120, 50, Color.YELLOW, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    isBGBlack = false;
-                    isBGWhite = true;
+                if (isBGWhite == true && isBGBlack == false) {
+                    setBackground(Color.WHITE);
+
+                    // bottom pad
+                    RenderPaddles bottomPaddle = new RenderPaddles(bottomPadX, height - padH - inset, padW, padH, Color.WHITE, g);
+                    bottomPaddle.renderPaddle();
+
+                    // top pad
+                    RenderPaddles topPaddle = new RenderPaddles(topPadX, inset, padW, padH, Color.WHITE, g);
+                    topPaddle.renderPaddle();
+
+                    // ball
+                    DrawUtils.drawCircle(ballX, ballY, ballSize, Color.WHITE, g);
+
+                    TextUtils.drawText(BORDER, 0, height / 2, 35, Color.BLACK, g);
+                    String scoreT = "AI: " + new Integer(scoreTop).toString();
+                    String scoreB = "Player: " + new Integer(scoreBottom).toString();
+                    TextUtils.drawText(scoreT, width - 105, height / 2 - 30, 30, Color.BLACK, g);
+                    TextUtils.drawText(scoreB, width - 125, height / 2 + 30, 30, Color.BLACK, g);
                 }
-            }, this);
 
-            // black button
-            DrawUtils.drawBlackButton("BLACK", 10, height / 2 - 150, 120, 50, Color.YELLOW, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (isBGWhite == true) {
-                        isBGWhite = false;
-                        isBGBlack = true;
-                    } else {
-                        isBGBlack = true;
+                // reset button
+                DrawUtils.drawResetButton("RESET", 10, height / 2 + 30, 120, 50, Color.YELLOW, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        ballX = width / 2 - ballSize / 2;
+                        ballY = height / 2 - ballSize / 2;
+
+                        ballX += ballSpeedX;
+                        ballY += ballSpeedY;
+
+                        scoreBottom = 0;
+                        scoreTop = 0;
                     }
-                }
-            }, this);
+                }, this);
 
-            // player name
-            DrawUtils.drawPlayerNameBox("Put player name here", 1, 10, 100, 50, Color.BLUE, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                }
-            }, this);
+                // exit button
+                DrawUtils.drawExitButton("EXIT", 10, height / 2 + 90, 120, 50, Color.YELLOW, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.exit(5);
+                    }
+                }, this);
 
-            // game reset
-            if (isGameReset == true) {
-                TextUtils.drawText("Press the button to reset the ball.", width / 2, 150 * 2, 40, Color.WHITE, g);
-            }
-        }
+                // white button
+                DrawUtils.drawWhiteButton("WHITE", 10, height / 2 - 90, 120, 50, Color.YELLOW, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        isBGBlack = false;
+                        isBGWhite = true;
+                    }
+                }, this);
+
+                // black button
+                DrawUtils.drawBlackButton("BLACK", 10, height / 2 - 150, 120, 50, Color.YELLOW, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (isBGWhite == true) {
+                            isBGWhite = false;
+                            isBGBlack = true;
+                        } else {
+                            isBGBlack = true;
+                        }
+                    }
+                }, this);
+
+                // player name
+                DrawUtils.drawPlayerNameBox("Put player name here", 1, 10, 100, 50, Color.BLUE, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                    }
+                }, this);
+
+                // game reset
+                if (isGameReset == true) {
+                    TextUtils.drawText("Press the button to reset the ball.", width / 2, 150 * 2, 40, Color.WHITE, g);
+                }
+            }        }
     }
 
     @Override
---
+++ src/main/java/me/maxish0t/pingpong/gui/VariablesGUI.java
@@ -0,0 +1,13 @@
+package me.maxish0t.pingpong.gui;
+
+public class VariablesGUI {
+
+    public static float fade;
+    public static int   timer;
+
+    static {
+        fade = 1.0F;
+        timer = 190;
+    }
+
+}
